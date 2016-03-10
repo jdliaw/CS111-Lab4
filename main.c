b4b245b0e8c74dcefa786d4f585a34bd3ccef821
jdliaw <jennifer.liaw07@gmail.com>
2016-03-09 22:21:09 -0800

    added sync option

--- main.c
+++ main.c
@@ -39,21 +39,10 @@ void* thread_func(void* arg) {
 	}
 }
 
-/* addtest:
-takes a parameter for the number of parallel threads (--threads=#, default 1)
-takes a parameter for the number of iterations (--iterations=#, default 1)
-initializes a (long long) counter to zero.
-notes the (high resolution) starting time for the run (using clock_gettime(2))
-starts the specified number of threads, each of which will use the above add function to:
-	add 1 to the counter the specified number of times
-	add −1 to the counter the specified number of times
-	exits to re-join the parent thread
-wait for all threads to complete, and notes the (high resolution) ending time for the run.
-checks to see if the counter value is zero, and if not log an error message to stderr
-prints to stdout
-	the total number of operations performed
-	the total run time (in nanoseconds), and the average time per operation (in nanoseconds).
-If there were no errors, exit with a status of zero, else a non-zero status */
+/* addtest with pthread_mutex */
+void m_addtest(long nthreads, long niter) {
+
+}
 
 void addtest(long nthreads, long niter) {
 	int exit_status = 0;
@@ -194,6 +183,19 @@ int main(int argc, char **argv) {
 	  	}
 
  	}
- 	addtest(nthreads, iterations);
+ 	// run appropriate protected add functions given sync option
+ 	if (sync == 'm') {
+ 		m_addtest(nthreads, iterations);
+ 	}
+ 	else if (sync == 's') {
+
+ 	}
+ 	else if (sync == 'c') {
+
+ 	}
+ 	else {
+ 		addtest(nthreads, iterations);
+ 	}
+
  	return 0;
 }
