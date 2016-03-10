c7ce436bed4edc590fbbdfbeee2b9fe4181f42f0
Brandon Liu <brandonliu@s-128-97-104-197.resnet.ucla.edu>
2016-03-09 23:00:09 -0800

    added sync test lock and set or whatever

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 // CS 111 Lab 4
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -8,92 +9,114 @@
 
 /* Part 1 */
 
+static long long counter;
+static pthread_mutex_t lock;
+
+volatile int lock_s = 0;
+
+int opt_yield;
+char sync;
+
 struct Threadargs {
 	long long counter;
-	int iterations;
+	long iterations;
 };
 
 void add(long long *pointer, long long value) {
     long long sum = *pointer + value;
+    if (opt_yield)
+            pthread_yield();
     *pointer = sum;
 }
 
-void* thread_func(void* arg) {
-	struct Threadargs *args = (struct Threadargs*)arg;
-	long long counter = args->counter;
-	int iterations = args->iterations;
-	int i;
+void* count(void* arg) {
+  long iterations = (long)arg;
+  long i;
 	for (i = 0; i < iterations; i++) {
 		add(&counter, 1);
 	}
 	for (i = 0; i < iterations; i++) {
 		add(&counter, -1);
 	}
-	// TODO: exit to re-join parent thread?
 }
 
-/* addtest:
-takes a parameter for the number of parallel threads (--threads=#, default 1)
-takes a parameter for the number of iterations (--iterations=#, default 1)
-initializes a (long long) counter to zero.
-notes the (high resolution) starting time for the run (using clock_gettime(2))
-starts the specified number of threads, each of which will use the above add function to:
-	add 1 to the counter the specified number of times
-	add −1 to the counter the specified number of times
-	exits to re-join the parent thread
-wait for all threads to complete, and notes the (high resolution) ending time for the run.
-checks to see if the counter value is zero, and if not log an error message to stderr
-prints to stdout
-	the total number of operations performed
-	the total run time (in nanoseconds), and the average time per operation (in nanoseconds).
-If there were no errors, exit with a status of zero, else a non-zero status */
+/* addtest with pthread_mutex */
+void* mutex_count(void* arg) {
 
-void addtest(int nthreads, int niter) {
-	// TODO: nthreads, niter default = 1
+}
+
+void* s_count(void*arg) {
+  long iterations = (long)arg;
+  long i;
+  for(i = 0; i < iterations; i++) {
+    while(__sync_lock_test_and_set(&lock_s, 1));
+    add(&counter, 1);
+    __sync_lock_release(&lock_s, 1);
+  }
+
+  for(i = 0; i < iterations; i++) {
+    while(__sync_lock_test_and_set(&lock_s, 1));
+    add(&counter, -1);
+    __sync_lock_release(&lock_s, 1);
+  }
+}
+
+void addtest(long nthreads, long niter) {
 	int exit_status = 0;
-	long long counter = 0;
-	pthread_t *threads;
-	//clock_t clk = CLOCK_MONOTONIC;
-	struct timespec *tp;  /* time_t tv_sec; // whole secs >= 0
+	counter = 0;
+	struct timespec tp;  /* time_t tv_sec; // whole secs >= 0
 							long tv_nsec; // nanoseconds */
-	int clock_ret = clock_gettime(CLOCK_MONOTONIC, tp); // TODO: not sure what clk_id should be.. CLOCK_REALTIME?
+	int clock_ret = clock_gettime(CLOCK_MONOTONIC, &tp);
 	if (clock_ret != 0) {
-		//TODO: error handling
+	  fprintf(stderr, "Error in clock_gettime()\n");
+	  exit_status = 1;
 	}
-	long start_time = tp->tv_nsec; // want "high resolution" aka in ns
+
+	long start_time = tp.tv_nsec; // want "high resolution" aka in ns
+	fprintf(stderr, "start_time: %lu\n", start_time);
 
 
 	//malloc threads
 	pthread_t *tids = malloc(nthreads * sizeof(pthread_t));
 
-	// struct for thread args
-	struct Threadargs *args;
-	args->counter = counter;
-	args->iterations = niter;
-
 	// start threads
 	unsigned i;
+	int thread_ret;
 	for (i = 0; i < nthreads; i++) {
-		int thread_ret = pthread_create(&tids[i], NULL, thread_func, (void*)args);
+		// depending on sync option, run corresponding thread function
+		if (sync == 'm') {
+			thread_ret = pthread_create(&tids[i], NULL, mutex_count, (void*)niter);
+		}
+		else if (sync == 's') {
+      			thread_ret = pthread_create(&tids[i], NULL, s_count, (void*)niter);
+		}
+		else if (sync == 'c') {
+			thread_ret = pthread_create(&tids[i], NULL, count, (void*)niter);
+			}
+		else {
+			thread_ret = pthread_create(&tids[i], NULL, count, (void*)niter);
+			}
+		// error handling
 		if (thread_ret != 0) {
-			// TODO: error handling
-			exit_status = 1;
+		  fprintf(stderr,"Error creating threads\n");
+		  exit_status = 1;
 		}
 	}
 
 	// wait for threads to complete, join.
 	for (i = 0; i < nthreads; i++) {
-		int thread_ret = pthread_join(tids[i], NULL); // TODO: not sure how this retval arg works
+		thread_ret = pthread_join(tids[i], NULL);
 		if(thread_ret != 0) {
+		  fprintf(stderr, "Error joining threads\n");
 			exit_status = 1;
 		}
 	}
 
 	// get ending time for run
-	clock_gettime(CLOCK_MONOTONIC, tp);
-	long end_time = tp->tv_nsec;
+	clock_gettime(CLOCK_MONOTONIC, &tp);
+	long end_time = tp.tv_nsec;
 	long elapsed_time = end_time - start_time;
-	int noperations = nthreads * niter * 2;
+	long noperations = nthreads * niter * 2;
 	long average_time = elapsed_time / noperations;
 
 	// error message if counter not zero
@@ -104,20 +127,25 @@ void addtest(int nthreads, int niter) {
 	}
 
 	// print stuff to stdout
-	fprintf(stdout, "%d threads x %d iterations x (add + subtract) = %d operations\n", nthreads, niter, noperations);
-	fprintf(stdout, "elapsed time: %lu\n", elapsed_time);
-	fprintf(stdout, "per operation: %lu\n", average_time);
+	fprintf(stdout, "%lu threads x %lu iterations x (add + subtract) = %lu operations\n", nthreads, niter, noperations);
+	fprintf(stdout, "elapsed time: %lu ns\n", elapsed_time);
+	fprintf(stdout, "per operation: %lu ns\n", average_time);
 
-	// TODO: exit non-zero status if errors, exit 0 if no errors.
+	// exit non-zero status if errors, exit 0 if no errors.
 	exit(exit_status);
 }
 
 int main(int argc, char **argv) {
+	long nthreads = 0;
+	long iterations = 0;
+
 	while (1) {
 	    static struct option long_options[] =
 	    {
 		    { "threads", optional_argument, 0, 't' },
 			{ "iter", optional_argument, 0, 'i' },
+			{ "sync", optional_argument, 0, 's' },
+			{ "yield", optional_argument, 0, 'y' },
 			{ 0, 0, 0, 0 }
 	    };
 	    int option_index = 0;
@@ -128,40 +156,62 @@ int main(int argc, char **argv) {
 	      break;
 	    }
 
-	    int nthreads = 0;
-	    int iterations = 0;
-
-	    fprintf(stderr, "before switch\n");
-
 	    switch (c)
 	    {
 	      	/* nthreads */ 
 	    	case 't':
 	    		if(optarg) {		//threads=<something>
 	    			nthreads = atoi(optarg);
-	    			fprintf(stderr, "threads=%d\n", nthreads);
+	    			fprintf(stderr, "threads=%lu\n", nthreads);
 	    		}
 	    		else {
 	    			nthreads = 1;	//default 1
-	    			fprintf(stderr, "threads=%d\n", nthreads);
+	    			fprintf(stderr, "threads=%lu\n", nthreads);
 	    		}
 		      	break;
 	      	/* niterations */
 	      	case 'i':
 				if(optarg) {
 					iterations = atoi(optarg);
-					fprintf(stderr, "iter=%d\n", iterations);
+					fprintf(stderr, "iter=%lu\n", iterations);
 				}
 				else {
 					iterations = 1;
-					fprintf(stderr, "iter=%d\n", iterations);
+					fprintf(stderr, "iter=%lu\n", iterations);
 				}
+	      		break;
+	      	/* sync option
+	      		m = mutex
+	      		s = spinlock
+	      		c = compare and swap */
+	      	case 's':
+	      		if (optarg) {
+	      			sync = *optarg;
+	      			fprintf(stderr, "sync=%c\n", sync);
+	      		}
+	      		else {
+	      			fprintf(stderr, "Invalid sync option\n");
+	      			exit(1);
+	      		}
+	      		break;
+	      	/* yield option */
+	      	case 'y':
+	      		if (optarg) {
+	      			opt_yield = atoi(optarg);
+	      			fprintf(stderr, "yield=%d\n", opt_yield);
+	      		}
+	      		else {
+	      			opt_yield = 1;
+	      			fprintf(stderr, "yield=%d\n", opt_yield);
+	      		}
 	      		break;
 	      	default: 
 	      		break;
 	  	}
 
  	}
+
  	addtest(nthreads, iterations);
+
  	return 0;
 }
